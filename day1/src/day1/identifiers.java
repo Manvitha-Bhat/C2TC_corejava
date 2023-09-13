95b03a8d164960e3d469565c1d0229530800a822
Manvitha-Bhat <manvitha.20cs043@sode-edu.in>
2023-09-13 18:22:55 +0530

    first commit

---
+++ day1/src/day1/identifiers.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class identifiers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char a='\u00A7';
+		System.out.println(a);
+
+	}
+
+}
